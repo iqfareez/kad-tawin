5abb0456ffe71101b3d83f4b3b1ba4a711d2fce4
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-06-01 23:12:06 +0800

    :sparkles: enhance navigation nav

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="text-left">
-    <span class="font-playfair text-2xl font-bold text-gray-800">Tawin.my</span>
-    <div class="text-xs text-purple-600 font-semibold">Kad Kahwin Digital</div>
+    <span class="font-playfair text-2xl font-bold text-gray-800 dark:text-gray-100">Tawin.my</span>
+    <div class="text-xs text-purple-600 dark:text-purple-400 font-semibold">Kad Kahwin Digital</div>
 </div>
